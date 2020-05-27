420fac5b9257fb807d32a1e436469263ae660911
Aleksandr <asletkin@mail.ru>
2020-05-27 22:40:40 +0300

    2705202240

--- function.js
+++ function.js
@@ -25,17 +25,47 @@
 
 // 3 Стрелочные функции
 
-function greet(name) {
-    console.log('Hello - ', name)
-}
+// function greet(name) {
+//     console.log('Hello - ', name)
+// }
 
-const arrow = (name) => {
-    console.log('Привет - ', name)
-}
+// const arrow = (name) => {
+//     console.log('Привет - ', name)
+// }
 
-arrow('Alex')
+// arrow('Alex')
 
-const arrow2 = name => console.log('Привет - ', name)
+// const arrow2 = name => console.log('Привет - ', name)
 
-arrow2('Maxim')
+// arrow2('Maxim')
+
+// 4 Параметры по умолчанию
+
+// const sum = (a, b) => a + b
+// console.log(sum(41, 1))
+
+// function sumAll(...all) {
+// let result = 0
+// for (let num of all) {
+//     result += num
+// }
+// return result
+// }
+// const res = sumAll(1, 2, 3, 4, 5, 6)
+// console.log(res)
+
+// 5 Замыкания
+
+// function createMember(name) {
+//     return function(lastName) {
+//         console.log(name + lastName)
+//     }
+// }
+
+// const LogWithLastName = createMember('Alex ')
+// console.log(LogWithLastName('Slet'))
+// console.log(LogWithLastName('Sletkin'))
+
+
+
 
