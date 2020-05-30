4109b37b47e132662875aa4ba29e396cef78da80
Aleksandr <asletkin@mail.ru>
2020-05-30 14:44:14 +0300

    3005201445

---
+++ massive.js
@@ -0,0 +1,12 @@
+const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес']
+const fib = [1, 1, 2, 3, 5, 8, 13]
+
+// Function
+function addItemToEnd(){
+
+}
+
+// Method
+cars.push('Порше')
+cars.unshift('Волга')
+console.log(cars)
